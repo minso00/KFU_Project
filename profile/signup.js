eb637410c5ed248bc85f4cc53ad0e6156b933e93
minso00 <105704159+minso00@users.noreply.github.com>
2024-06-30 22:00:07 +0900

    index 수정 및 회원가입 수정

--- profile/signup.js
+++ profile/signup.js
@@ -22,7 +22,7 @@ $(document).ready(function(){
   
 
  //----------- sign in ---------------------
- signin.on("click",function(e){
+signin.on("click",function(e){
     e.preventDefault();
     $(this).parent().parent().siblings("h1").text("SIGN IN");
     $(this).parent().css("opacity","1");
@@ -35,11 +35,47 @@ $(document).ready(function(){
     });
     signin_btn.text("Sign in");
   });
- 
+
  //----------- reset ---------------------
- reset.on("click",function(e){
-   e.preventDefault();
-   $(this).parent().parent().siblings("form")
-   .find(".input__block").find(".input").val("");
- })
+reset.on("click",function(e){
+  e.preventDefault();
+  $(this).parent().parent().siblings("form")
+  .find(".input__block").find(".input").val("");
+})
 });
+
+//--------------phone number-----------
+function autoHypenPhone(str){
+  str = str.replace(/[^0-9]/g, '');
+  var tmp = '';
+  if( str.length < 4){
+      return str;
+  }else if(str.length < 7){
+      tmp += str.substr(0, 3);
+      tmp += '-';
+      tmp += str.substr(3);
+      return tmp;
+  }else if(str.length < 11){
+      tmp += str.substr(0, 3);
+      tmp += '-';
+      tmp += str.substr(3, 3);
+      tmp += '-';
+      tmp += str.substr(6);
+      return tmp;
+  }else{              
+      tmp += str.substr(0, 3);
+      tmp += '-';
+      tmp += str.substr(3, 4);
+      tmp += '-';
+      tmp += str.substr(7);
+      return tmp;
+  }
+  return str;
+}
+
+var cellPhone = document.getElementById('cellPhone');
+cellPhone.onkeyup = function(event){
+event = event || window.event;
+var _val = this.value.trim();
+this.value = autoHypenPhone(_val) ;
+}
